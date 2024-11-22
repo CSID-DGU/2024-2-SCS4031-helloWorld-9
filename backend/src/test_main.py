36e738e15fba9f0f23f2a7b3574c2346cbdc312e
Jaeho Jeon <88sprout@gmail.com>
2024-11-22 14:28:44 +0900

    test: 테스트 파일 경로 수정

--- backend/src/test_main.py
+++ backend/src/test_main.py
@@ -3,7 +3,7 @@ from retrieval import Retriev_Gen
 
 if __name__ == "__main__":
     embedder = Embedder()
-    embedder.add_docs("C:/Users/hj780/Desktop/2024-2-SCS4031-helloWorld-9/backend/data/한화생명 간편가입 시그니처 암보험(갱신형) 무배당_2055-001_002_약관_20220601_.pdf")
+    embedder.add_docs("hanhwa-testdata.pdf")
 
     responser = Retriev_Gen()
     
