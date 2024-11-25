e2957999703f1b0647720dc6f6e2e115b5e0c3f6
Jaeho Jeon <88sprout@gmail.com>
2024-11-26 04:55:03 +0900

    chore: 프론트엔드 개발서버의 포트번호를 명시

--- frontend/my-svelte-app/vite.config.js
+++ frontend/my-svelte-app/vite.config.js
@@ -8,13 +8,14 @@ dotenv.config();
 export default defineConfig({
   plugins: [svelte()],
   server: {
+    port: 3000, // Vite 서버는 3000번 포트
     proxy: {
+      // API 요청을 FastAPI로 프록시
       '/api': {
-        target: 'http://localhost:8000', // 백엔드 서버 주소
+        target: 'http://127.0.0.1:8000', // FastAPI가 실행 중인 주소
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
+      }
+    }
   },
   resolve: {
     alias: {
