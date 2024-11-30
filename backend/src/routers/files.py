65e74d3c9cc7608c5a182eeb64b552529e68a231
Jaeho Jeon <88sprout@gmail.com>
2024-11-30 19:01:21 +0900

    WIP : pdf 파일을 두개 연속으로 삭제하면 Race Confition 으로 인해 오류가 발생

--- backend/src/routers/files.py
+++ backend/src/routers/files.py
@@ -13,6 +13,24 @@ logger = logging.getLogger(__name__)
 root_upload_path = f"{BASE_DIR}/uploads"
 embedder = Embedder(db_path=DB_PATH)
 
+
+# Todo : vectorDB 삭제
+# 동기로 작동해야하는 부분을 def 로 구현
+def vector_db_restore():
+    # Todo : vectorDB 제거 후, 나머지 pdf 다시 임베딩
+
+
+    # pdf_file 을 모두 읽어서 vectorDB 복구
+    pdf_files = [f for f in os.listdir(root_upload_path) if f.endswith('.pdf')]
+    for pdf_file in pdf_files:
+        pdf_path = os.path.join(root_upload_path, pdf_file)
+        logger.info(f"start embedding : {pdf_path}")
+        # embedding 수행
+        embedder.add_docs(pdf_path)
+        logger.info(f"embeding complete : {pdf_path}")
+    logger.info(f"VectorDB restored after file delete : {pdf_path}")
+
+
 @router.get("/files")
 async def loadfile_test(directory: str = root_upload_path):
     """
@@ -55,19 +73,7 @@ async def delete_uploaded_file(ids: str = Body(...)):  # str 타입으로 받음
             logger.info(f"Deleted file: {file_to_delete}")
         else:
             logger.warning(f"File does not exist or is not a file: {file_to_delete}")
-        
-    # Todo : vectorDB 제거 후, 나머지 pdf 다시 임베딩
 
-    # Todo : vectorDB 삭제
-
-    # pdf_file 을 모두 읽어서 vectorDB 복구
-    pdf_files = [f for f in os.listdir(root_upload_path) if f.endswith('.pdf')]
-    for pdf_file in pdf_files:
-        pdf_path = os.path.join(root_upload_path, pdf_file)
-        logger.info(f"start embedding : {pdf_path}")
-        # embedding 수행
-        embedder.add_docs(pdf_path)
-        logger.info(f"embeding complete : {pdf_path}")
-    logger.info(f"VectorDB restored after file delete : {pdf_path}")
+        vector_db_restore()
 
     return {"message": "Files deletion completed", "deleted_files": ids}
