3896111270a9ca4f62b6b93dc764bca94a80bfee
Jaeho Jeon <88sprout@gmail.com>
2024-11-30 15:10:21 +0900

    feat: 임베딩 엔드포인트 생성 및 테스트 완료

--- backend/src/routers/upload.py
+++ backend/src/routers/upload.py
@@ -1,8 +1,22 @@
 from fastapi import APIRouter, UploadFile, File
 import os
 import shutil
+import logging
 
 router = APIRouter()  # APIRouter 생성
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
+
+# FastAPI 의 router function 에서, async def 와 def 는 실행방식의 차이가 있음.
+# async def 를 쓰면 single thread event loop 로 처리함. IO bound 에 유리하고 CPU bound 에 불리함.
+# 따라서, IO bound 인 upload_test 는 async def 로 구현,
+# CPU bound 인 embed_pdf 는 def 로 구현하였음
+
+def embed_pdf(filepath):
+    logger.debug(f"start embedding : {filepath}")
+    # embedding 수행
+
 
 @router.post("/upload")
 async def upload_test(file: UploadFile = File(...), id: str = "/"):
@@ -15,5 +29,7 @@ async def upload_test(file: UploadFile = File(...), id: str = "/"):
 
     with open(save_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
+
+    embed_pdf(save_path)
 
     return {"filename": file.filename, "saved_to": save_path}
