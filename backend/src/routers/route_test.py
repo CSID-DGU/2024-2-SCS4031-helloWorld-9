d6edafb91efb3e9b59d1ca4f722129c7fe56b6c1
Jaeho Jeon <88sprout@gmail.com>
2024-11-30 15:08:20 +0900

    test: 로그 시스템 추가
    오류를 디버깅하기 쉽도록 uvicorn 과 잘 연동되는 로그 라이브러리를 연동했습니다

--- backend/src/routers/route_test.py
+++ backend/src/routers/route_test.py
@@ -2,12 +2,19 @@ from fastapi import APIRouter, Query
 from fastapi.responses import JSONResponse
 from typing import Optional
 import shutil
+import logging
+
 
 router = APIRouter()  # APIRouter 생성
+
+# 로그 테스트
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
 
 @router.get("/check")
 async def tester():
     test_json = {
         "test":"OK"
     }
+    logger.debug(f"loggin system works...")
     return JSONResponse(content=test_json)
