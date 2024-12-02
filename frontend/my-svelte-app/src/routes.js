e4978c21c715ed4f0082c7b1ed93a2deece9f359
김영찬 <gim-yeongchan@gim-yeongchan-ui-MacBookAir.local>
2024-12-02 21:29:41 +0900

    feat: Graphview_frontend 기초작업

--- frontend/my-svelte-app/src/routes.js
+++ frontend/my-svelte-app/src/routes.js
@@ -1,7 +1,9 @@
-import PdfManager from './routes/Pdfmanager.svelte';
-import Chatbot from './routes/Chatbot.svelte';
+import PdfManager from "./routes/Pdfmanager.svelte";
+import Chatbot from "./routes/Chatbot.svelte";
+import GraphView from "./routes/GraphView.svelte";
 
 export default {
-  '/pdfmanager': PdfManager,
-  '/chatbot': Chatbot,
+  "/pdfmanager": PdfManager,
+  "/chatbot": Chatbot,
+  "/graphview": GraphView,
 };
