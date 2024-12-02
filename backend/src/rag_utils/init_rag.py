4a540928346300252d0a2457dece1ac451aedeeb
Jaeho Jeon <88sprout@gmail.com>
2024-12-02 22:15:28 +0900

    feat: chatbot 이 이미 업로드된 파일들을 처음 시작 시 임베딩하는 기능을 추가

---
+++ backend/src/rag_utils/init_rag.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+from typing import List, Optional
+import logging
+from rag_utils.embedding import Embedder  # VectorDB 초기화에 필요
+from rag_utils.retrieval import Retriever  # RAG 시스템
+from pathlib import Path
+
+logger = logging.getLogger("init_vectorDB")
+logging.basicConfig(level=logging.INFO,format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",)
+
+def try_init_vectorDB_from_uploads(db_path,upload_path):
+    # 이미 업로드된 PDF 파일 임베딩
+    try:
+        logger.info("trying init vectorDB from uploads")
+        embedder = Embedder(db_path)
+        pdf_files = [file for file in upload_path.rglob("*.pdf") if file.is_file()]
+
+        if not pdf_files:
+            logger.info("No PDF files found.")
+            # PDF 파일이 없는 경우 추가 작업
+        else:
+            logger.info(f"Found {len(pdf_files)} PDF files:")
+            for pdf in pdf_files:
+                logger.info(f"pdf 파일 임베딩 시작 : {pdf}")
+                embedder.add_docs(pdf)
+                logger.info(f"pdf 파일 임베딩 완료 : {pdf}")
+    except Exception as e:
+        logger.error(f"Failed to gen embedder: {str(e)}")
+        retriev_gen_error = e
+        responser = None
+
