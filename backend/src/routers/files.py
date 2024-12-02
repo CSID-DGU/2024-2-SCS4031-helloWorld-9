9b269ddd8732fe56f18b23ff1c32aa15d61631bf
Jaeho Jeon <88sprout@gmail.com>
2024-12-02 23:15:50 +0900

    feature: 파일 삭제 기능 구현 완료.
    파일 삭제 시 나머지 파일이 자동으로 재 임베딩됩니다.

--- backend/src/routers/files.py
+++ backend/src/routers/files.py
@@ -6,6 +6,7 @@ import logging
 from config import BASE_DIR, DB_PATH, UPLOAD_PATH
 import os
 from rag_utils.embedding import Embedder
+from rag_utils.init_rag import try_init_vectorDB_from_uploads
 from web_utils.file_manager import get_all_pdfs
 
 router = APIRouter()  # APIRouter 생성
@@ -13,23 +14,6 @@ logger = logging.getLogger(__name__)
 
 root_upload_path = f"{BASE_DIR}/uploads"
 embedder = Embedder(db_path=DB_PATH)
-
-
-# Todo : vectorDB 삭제
-# 동기로 작동해야하는 부분을 def 로 구현
-def vector_db_restore():
-    # Todo : vectorDB 제거 후, 나머지 pdf 다시 임베딩
-
-
-    # pdf_file 을 모두 읽어서 vectorDB 복구
-    pdf_files = [f for f in os.listdir(root_upload_path) if f.endswith('.pdf')]
-    for pdf_file in pdf_files:
-        pdf_path = os.path.join(root_upload_path, pdf_file)
-        logger.info(f"start embedding : {pdf_path}")
-        # embedding 수행
-        embedder.add_docs(pdf_path)
-        logger.info(f"embeding complete : {pdf_path}")
-    logger.info(f"VectorDB restored after file delete : {pdf_path}")
 
 
 @router.get("/files")
@@ -59,7 +43,7 @@ async def loadfile_test(directory: str = root_upload_path):
 # async def 를 사용하니, 비동기로 인해 race condition 이 유발되는 듯 함. (존재하지 않는 파일을 삭제하려고 시도하여, 프로그램이 죽음)
 ## try catch 등의 명시적 핸들링을 작성하여 존재하지 않는 파일을 삭제하려고 시도해도 핸들링되거나,
 ## def (동기함수) 로 재구현해서 오류를 해결해야함.
-'''
+
 
 @router.delete("/files")
 async def delete_uploaded_file(ids: str = Body(...)):  # str 타입으로 받음
@@ -80,8 +64,8 @@ async def delete_uploaded_file(ids: str = Body(...)):  # str 타입으로 받음
             logger.info(f"Deleted file: {file_to_delete}")
         else:
             logger.warning(f"File does not exist or is not a file: {file_to_delete}")
-
-        vector_db_restore()
+    
+    # 파일 삭제 시, 나머지 파일을 재 임베딩 시도
+    try_init_vectorDB_from_uploads(db_path=DB_PATH,upload_path=UPLOAD_PATH)
 
     return {"message": "Files deletion completed", "deleted_files": ids}
-'''
