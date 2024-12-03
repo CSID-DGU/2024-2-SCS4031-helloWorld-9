2588d57e3dd49761472df0a9f8edcef54b6f9c19
Jaeho Jeon <88sprout@gmail.com>
2024-12-03 13:39:09 +0900

    fix: graphView merge시 삭제된 app.py 복구

--- frontend/my-svelte-app/src/routes.js
+++ frontend/my-svelte-app/src/routes.js
@@ -1,6 +1,6 @@
 import PdfManager from "./routes/Pdfmanager.svelte";
 import Chatbot from "./routes/Chatbot.svelte";
-import GraphView from "./routes/GraphView.svelte";
+import GraphView from "./routes/Graphview.svelte";
 
 export default {
   "/pdfmanager": PdfManager,
